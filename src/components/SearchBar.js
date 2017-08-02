63701f40d8a57edf1f7e903a0bbad4699ddc2d26
Ericka Emery <ericka@elonandcompany.com>
2017-08-02 04:48:02 -0500

    class component practice

---
+++ src/components/SearchBar.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+// const SearchBar = () => {
+//   return (
+//     <input />
+//   );
+// }
+
+// refactor from functional to class component.
+// class components to record keeping and communicating with other components
+
+// every class has a render. a function. that will return JSX, always.
+
+
+class SearchBar extends Component {
+  render () {
+    return (
+    <input />
+    );
+  }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import SearchBar from './components/searchBar';
+
+const API_Key = 'AIzaSyACCtbLbcZKi3jP_F4stQr0A_lE1UkDn_Q';
 // importing react library here. go find the lbrary called react and assign it to the variable react
 
 
@@ -11,10 +15,14 @@ import ReactDOM from 'react-dom';
 
 // Using a fat arrow instead. It helps to have more compact syntaxin React.
 const App = () => {
-  return <div>Herro!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Take the component's generated HTML and put it on the page (in the DOM)
 
-ReactDOM.render(<App/>, document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
 // Line above doesn't work alone because I'm not importing React library. Nothing in HTML either.
